4d5386cd53e97756fb1aa11fea5559632f494cfb
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2023-01-28 15:23:12 +0300

    Update do.py

--- do.py
+++ do.py
@@ -1,17 +1,5 @@
 ''' Run a function by ado <func_name> '''
 
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.types.web_app_info import WebAppInfo
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import callback_query
-import requests,user_agent,json,os,sys,secrets,names,urllib
-import random, datetime
-from faker import Faker 
-from time import sleep
-from user_agent import generate_user_agent
-
 def set_hook():
     import asyncio
     from bot.settings import HEROKU_APP_NAME, WEBHOOK_URL, BOT_TOKEN
