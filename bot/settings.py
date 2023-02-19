1b3ba9e3c88dcb0d3bf6bf2ab58034bd6fa38387
DEAD CODE <90526870+TeamArmx@users.noreply.github.com>
2023-02-19 10:11:14 +0200

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -6,11 +6,11 @@ if not BOT_TOKEN:
     print('You have forgot to set BOT_TOKEN')
     quit()
 
-HEROKU_APP_NAME = 'onclikk'
+HEROKU_APP_NAME = 'heroic-credit'
 
 
 # webhook settings
-WEBHOOK_HOST = f'https://onclikk.herokuapp.com'
+WEBHOOK_HOST = f'https://aiogram-heroku-production.up.railway.app'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
