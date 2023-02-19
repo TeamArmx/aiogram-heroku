81a87bfa0b234f9fd645525e17c7fd608aa347fc
DEAD CODE <90526870+TeamArmx@users.noreply.github.com>
2023-02-19 10:04:55 +0200

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-BOT_TOKEN = '5734388256:AAHFS1seyZNDE3Lxr_R28v6qCaijrN1tyw8' 
+BOT_TOKEN = '6262738056:AAEfRm1yPP5vc9aZ3I_a7g1RWKPgPqcHuDA' 
 if not BOT_TOKEN:
     print('You have forgot to set BOT_TOKEN')
     quit()
